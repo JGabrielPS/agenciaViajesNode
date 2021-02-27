3cd03d7e8fdd101da7801a8883610b88eaf03b1c
Gabriel Perez <juan.gabriel.ps@gmail.com>
2021-02-26 23:02:24 -0300

    addition: added server file

---
+++ index.js
@@ -0,0 +1,29 @@
+import express from "express";
+import router from "./routes/index.js";
+import db from "./config/db.js";
+
+const app = express();
+
+db.authenticate()
+  .then(() => console.log("DB conectada"))
+  .catch((error) => console.log(error));
+
+app.set("view engine", "pug");
+
+app.use((req, res, next) => {
+  const year = new Date();
+  res.locals.currentYear = year.getFullYear();
+  res.locals.nombreSitio = "Agencia de Viajes";
+
+  return next();
+});
+
+app.use(express.static("public"));
+
+app.use("/", router);
+
+const port = process.env.PORT || 4000;
+
+app.listen(port, () => {
+  console.log(`El servidor esta escuchando en el puerto ${port}`);
+});
