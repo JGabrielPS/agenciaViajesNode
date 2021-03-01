0fbb7dbcf32e59c84bd811ba820349e210813037
Juan Gabriel Pérez <juan.gabriel.ps@gmail.com>
2021-03-01 13:10:51 -0300

    addition: added testimoniales post endpoint and controller

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,7 @@ import {
   paginaTestimoniales,
   paginaDetalleViaje,
 } from "../controllers/pageController.js";
+import { guardarTestimonial } from "../controllers/testimonialController.js";
 
 const router = express.Router();
 
@@ -14,8 +15,10 @@ router.get("/", paginaInicio);
 router.get("/nosotros", paginaNosotros);
 
 router.get("/viajes", paginaViajes);
+
 router.get("/viajes/:slug", paginaDetalleViaje);
 
 router.get("/testimoniales", paginaTestimoniales);
+router.post("/testimoniales", guardarTestimonial);
 
 export default router;
