f3fe24e85a7dc7f257cc9376f2004fdd9da904a3
Gabriel Perez <juan.gabriel.ps@gmail.com>
2021-03-01 16:42:31 -0300

    addition: added testimoniales data from db to testimoiales and inicio controllers. In this also added viajes data

--- controllers/pageController.js
+++ controllers/pageController.js
@@ -1,9 +1,24 @@
 import { Viaje } from "../models/Viaje.js";
+import { Testimonial } from "../models/Testimonial.js";
 
-const paginaInicio = (req, res) => {
-  res.render("inicio", {
-    pagina: "Inicio",
-  });
+const paginaInicio = async (req, res) => {
+  const promisesDB = [];
+
+  promisesDB.push(Viaje.findAll({ limit: 3 }));
+  promisesDB.push(Testimonial.findAll({ limit: 3 }));
+
+  try {
+    const resultado = await Promise.all(promisesDB);
+
+    res.render("inicio", {
+      pagina: "Inicio",
+      clase: "home",
+      viajes: resultado[0],
+      testimoniales: resultado[1],
+    });
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 const paginaNosotros = (req, res) => {
@@ -21,10 +36,16 @@ const paginaViajes = async (req, res) => {
   });
 };
 
-const paginaTestimoniales = (req, res) => {
-  res.render("testimoniales", {
-    pagina: "Testimoniales",
-  });
+const paginaTestimoniales = async (req, res) => {
+  try {
+    const testimoniales = await Testimonial.findAll();
+    res.render("testimoniales", {
+      pagina: "Testimoniales",
+      testimoniales,
+    });
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 const paginaDetalleViaje = async (req, res) => {
